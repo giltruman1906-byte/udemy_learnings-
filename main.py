50ab100b4b29f562b435eec310c38803a44ad6a1
gil truman <giltruman@gils-MacBook-Pro.local>
2025-09-03 15:39:13 +0300

    add main python

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+# mymail = "giltruman1906@gmail.com"
+# to_mail = "liaba99@gmail.com"
+# my_password = "ygqbucjncpnlfftw" 
+# connection = smtplib.SMTP("smpt.gmail.com" , 587)
+# connection.starttls()   
+# connection.login(user =mymail , password=my_password)
+# connection.sendmail(from_addr= mymail ,to_addrs= to_mail , msg="Subject: Test\n\nhello")
+# connection.close()
+
+import smtplib
+
+mymail = "giltruman1906@gmail.com"
+to_mail = "liaba99@gmail.com"
+my_password = "ygqbucjncpnlfftw"
+connection = smtplib.SMTP("smtp.gmail.com", 587)
+connection.starttls()
+connection.login(user=mymail, password=my_password)
+connection.sendmail(from_addr=mymail, to_addrs=to_mail, msg="Subject: Test\n\nhello")
+connection.close()
